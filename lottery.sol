51ba864df9bd5ff5f6c04717abaaddb23c25098c
sasatee <sarvam2601@gmail.com>
2024-08-06 12:24:35 +0400

    lottery deploy

---
+++ lottery.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+
+
+
+contract Lottery{
+    address payable[] public players;
+    address public manager;
+
+    constructor(){
+        manager = msg.sender;
+
+    }
+
+    receive() external payable {
+
+        require(msg.value == 0.1 ether);
+        players.push(payable(msg.sender));
+     }
+
+     function getBalance() public view returns (uint){
+        require(msg.sender == manager);
+        return address(this).balance;
+     }
+
+     function random() public view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players.length)));
+     }
+}
